5f1cba896b9c43aed970c8395f8071b276df9a39
Ahtisham-01 <ehtisham.alphasquad@gmail.com>
2023-05-08 16:59:23 +0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,21 @@ const HeroSection = dynamic(() => import('@/components/herosection'), {
 const MicroManagement = dynamic(() => import('@/components/micromanagement'), {
   ssr: false,
 });
+const Mediocrity = dynamic(() => import('@/components/mediocrity'), {
+  ssr: false,
+});
+const SavingTheWorld = dynamic(() => import('@/components/SavingTheWorld'), {
+  ssr: false,
+});
+const Consultation = dynamic(() => import('@/components/consultation'), {
+  ssr: false,
+});
 // import HeroSection from '@/components/herosection'
 // import MicroManagement from '@/components/micromanagement'
 import React from 'react'
-import Mediocrity from '@/components/mediocrity'
-import SavingTheWorld from '@/components/SavingTheWorld'
-import Consultation from '@/components/consultation'
+// import Mediocrity from '@/components/mediocrity'
+// import SavingTheWorld from '@/components/SavingTheWorld'
+// import Consultation from '@/components/consultation'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
